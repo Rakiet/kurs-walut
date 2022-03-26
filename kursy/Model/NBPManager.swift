dfe1f737bce7595e73e09db8f4fbe9cbb7ee5f3c
Piotr Żakieta <piter5331@gmail.com>
2022-03-26 11:15:02 +0100

    add URLSession

--- kursy/Model/NBPManager.swift
+++ kursy/Model/NBPManager.swift
@@ -8,10 +8,39 @@
 import Foundation
 
 struct NBPManager{
-    let NBPURL = "http://api.nbp.pl/api/exchangerates/tables"
+    let NBPURL = "https://api.nbp.pl/api/exchangerates/tables"
     
     func fetchCurrency(tableName:String){
         let ulrString = "\(NBPURL)/\(tableName)"
+        performRequest(urlString: ulrString)
+        
+    }
+    
+    func performRequest(urlString: String){
+        //Create URL
+        if let url = URL(string: urlString) {
+            //Create a URLSession
+            let session = URLSession(configuration: .default)
+            
+            //Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data:response:error:))
+            
+            //Start task
+            task.resume()
+            
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        if error != nil{
+            print(error!)
+            return
+        }
+        
+        if let safeDate = data {
+            let dataString = String(data: safeDate, encoding: .utf8)
+            print(dataString)
+        }
         
     }
 }
