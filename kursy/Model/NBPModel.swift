435047f4481f65bae7339b4b2537831e169904fd
Piotr Żakieta <piter5331@gmail.com>
2022-03-26 15:13:09 +0100

    create a NBPModel

---
+++ kursy/Model/NBPModel.swift
@@ -0,0 +1,24 @@
+//
+//  NBPModel.swift
+//  kursy
+//
+//  Created by Piotr Żakieta on 26/03/2022.
+//
+
+import Foundation
+
+
+struct NBPModel{
+    let table: String
+    let no: String
+    let effectiveDate: String
+    let rates: [Rates]
+}
+
+struct RatesModel{
+    let currency: String
+    let code: String
+    let mid: Double
+}
+
+
