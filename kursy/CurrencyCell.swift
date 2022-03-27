ea85b0268f0317b4a208a8945f5fcded8a31a0ba
Piotr Żakieta <piter5331@gmail.com>
2022-03-27 10:04:09 +0200

    create sketch a cell

---
+++ kursy/CurrencyCell.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyCell.swift
+//  kursy
+//
+//  Created by Piotr Żakieta on 27/03/2022.
+//
+
+import UIKit
+
+class CurrencyCell: UITableViewCell {
+
+    @IBOutlet weak var currencyImageView: UIImageView!
+    @IBOutlet weak var currencyLabelView: UILabel!
+    
+    
+    
+}
