42a6c6e215bb001bfb5b7dc3afb3b240bdac3d1e
Piotr Żakieta <piter5331@gmail.com>
2022-03-29 15:22:05 +0200

    add comments and update prepareactivityIndicator

--- kursy/Model/Extra.swift
+++ kursy/Model/Extra.swift
@@ -8,24 +8,20 @@
 import Foundation
 import UIKit
 struct Extra {
-
-var activityIndicator: UIActivityIndicatorView?
-
+    
+    var activityIndicator: UIActivityIndicatorView?
+    
     mutating func prepareactivityIndicator(sentView: UIView){
-    if #available(iOS 13.0, *) {
-        self.activityIndicator = UIActivityIndicatorView(style: .large)
         
-        activityIndicator!.translatesAutoresizingMaskIntoConstraints = false
-        sentView.addSubview(activityIndicator!)
-        activityIndicator!.centerXAnchor.constraint(equalTo: sentView.centerXAnchor).isActive = true
-        activityIndicator!.centerYAnchor.constraint(equalTo: sentView.centerYAnchor).isActive = true
-    } else {
-        activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
-        activityIndicator?.color = UIColor.green
+        if #available(iOS 13.0, *) {//jezeli wersja iOS jest wyzsza niz 13 to utworzy ten UIActivityIndicatorView
+            activityIndicator = UIActivityIndicatorView(style: .large)
+        } else {//jezeli wersja iOS jest nizsza niz 13 to utworzy ten UIActivityIndicatorView w stylu .whiteLarge i zmieni kolor na zielony
+            activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
+            activityIndicator?.color = UIColor.green
+        }
         activityIndicator!.translatesAutoresizingMaskIntoConstraints = false
         sentView.addSubview(activityIndicator!)
         activityIndicator!.centerXAnchor.constraint(equalTo: sentView.centerXAnchor).isActive = true
         activityIndicator!.centerYAnchor.constraint(equalTo: sentView.centerYAnchor).isActive = true
     }
-}
 }
