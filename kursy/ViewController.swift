4c9b944ed26748c1ae3aeb0249957f79b09aa1c5
Piotr Żakieta <piter5331@gmail.com>
2022-03-29 13:14:18 +0200

    add prepareactivityIndicator

--- kursy/ViewController.swift
+++ kursy/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController, NBPManagerDelegate  {
     
     @IBOutlet weak var segmentControlOutlet: UISegmentedControl!
@@ -17,14 +18,20 @@ class ViewController: UIViewController, NBPManagerDelegate  {
 
     var nbpData: NBPModel?
     
+    var activityIndicator: UIActivityIndicatorView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //test URLSession
         nbpManager.delegate = self
         nbpManager.fetchCurrency(tableName: "a")
         tableView.delegate = self
         tableView.dataSource = self
         tableView.reloadData()
+        
+        // wywołanie fukcji odpowiedzialnej za stworzenie spinera ladowania
+        prepareactivityIndicator()
+        
+        self.activityIndicator!.startAnimating()
         
 //        nbpData = NBPModel(table: "a", no: "a", effectiveDate: "as", rates: [Rates(currency: "PL", code: "PL", mid: 2.90)])
         
@@ -32,20 +39,26 @@ class ViewController: UIViewController, NBPManagerDelegate  {
     
 
     @IBAction func segmentControlAction(_ sender: Any) {
-        switch segmentControlOutlet.selectedSegmentIndex{
-        case 0:
-            nbpManager.fetchCurrency(tableName: "a")
-        case 1:
-            nbpManager.fetchCurrency(tableName: "b")
-            
-        default:
-            break
+        DispatchQueue.main.async {
+            switch self.segmentControlOutlet.selectedSegmentIndex{
+            case 0:
+                self.activityIndicator!.startAnimating()
+                self.nbpManager.fetchCurrency(tableName: "a")
+            case 1:
+                self.activityIndicator!.startAnimating()
+                self.nbpManager.fetchCurrency(tableName: "b")
+                
+            default:
+                break
+            }
         }
+        
     }
     
        
  
     func didUpdateNBP(nbp: NBPModel){
+        
         nbpData = nbp
         print("?????????????????????????????????")
         print(nbp.table)
@@ -55,8 +68,32 @@ class ViewController: UIViewController, NBPManagerDelegate  {
             print(i.code)
             print("===========")
         }
-        tableView.reloadData()
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.activityIndicator!.stopAnimating()
+        }
+        
     }
+    
+    //fukcji odpowiedzialnej za stworzenie spinera ladowania
+    func prepareactivityIndicator(){
+        if #available(iOS 13.0, *) {
+            activityIndicator = UIActivityIndicatorView(style: .large)
+            
+            activityIndicator!.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(activityIndicator!)
+            activityIndicator!.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            activityIndicator!.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        } else {
+            activityIndicator = UIActivityIndicatorView(style: .whiteLarge)
+            activityIndicator!.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview(activityIndicator!)
+            activityIndicator!.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+            activityIndicator!.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        }
+    }
+    
+    
     
     
     
