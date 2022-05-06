e0c074bbc8b541e645640211d38aab50abfc411d
Piotr Żakieta <piter5331@gmail.com>
2022-05-06 11:20:25 +0200

    remove currencyCodeLabelView

--- kursy/Cell/CurrencyCell.swift
+++ kursy/Cell/CurrencyCell.swift
@@ -11,7 +11,6 @@ import UIKit
 class CurrencyCell: UITableViewCell {
 
     @IBOutlet weak var currencyImageView: UIImageView!
-    @IBOutlet weak var currencyCodeLabelView: UILabel!
     @IBOutlet weak var currencyDateLabelView: UILabel!
     @IBOutlet weak var currencyNameLabelView: UILabel!
     @IBOutlet weak var currencyValueLabelView: UILabel!
@@ -19,7 +18,6 @@ class CurrencyCell: UITableViewCell {
     //funkcja odpowiedzialna za przekazanie widokowi zmiennych do wyswietlenia
     func setCurrencyCell(data: Rates, date: String){
         currencyImageView.image = UIImage(named: data.code)
-        currencyCodeLabelView.text = data.code
         currencyDateLabelView.text = date
         currencyNameLabelView.text = data.currency.capitalized
         currencyValueLabelView.text = "1 \(data.code) = \(data.mid) PLN"
