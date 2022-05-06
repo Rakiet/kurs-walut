5f46dab42c4d6b970071cd7cb996164b7216f4d2
Piotr Żakieta <piter5331@gmail.com>
2022-05-06 14:58:45 +0200

    create delegate & protocol

--- kursy/Cell/CurrencyCell.swift
+++ kursy/Cell/CurrencyCell.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class CurrencyCell: UITableViewCell {
 
+    @IBOutlet weak var calcButton: UIButton!
     @IBOutlet weak var currencyImageView: UIImageView!
     @IBOutlet weak var currencyDateLabelView: UILabel!
     @IBOutlet weak var currencyNameLabelView: UILabel!
     @IBOutlet weak var currencyValueLabelView: UILabel!
+    
+    var dataRatesToCalc: Rates?
+    weak var delegate: CurrencyCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+            
+        // Add action to perform when the button is tapped
+        self.calcButton.addTarget(self, action: #selector(calcButtonTapped(_:)), for: .touchUpInside)
+      }
     
     //funkcja odpowiedzialna za przekazanie widokowi zmiennych do wyswietlenia
     func setCurrencyCell(data: Rates, date: String){
@@ -24,4 +36,24 @@ class CurrencyCell: UITableViewCell {
         
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+      }
+    
+    @IBAction func calcButtonTapped(_ sender: Any) {
+        if let dataRatesToCalc = dataRatesToCalc {
+            if let delegate = delegate {
+                    delegate.currencyCell(self, calcButtonTapped: dataRatesToCalc)
+                }
+        }
+        
+    }
+    
+    
+}
+
+protocol CurrencyCellDelegate: AnyObject {
+  func currencyCell(_ currencyCell: CurrencyCell, calcButtonTapped dataRatesToCalc: Rates)
 }
