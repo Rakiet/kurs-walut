b95707b0d1a53fbd162e16664959736c199ed1e0
Piotr Żakieta <piter5331@gmail.com>
2022-05-29 10:03:18 +0200

    add protocol DetailCellDelegate add target to button add showCalcButtonAction

--- kursy/Cell/DetailViewCell.swift
+++ kursy/Cell/DetailViewCell.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
+protocol DetailCellDelegate: AnyObject {
+  func detailCell(_ detailCell: DetailViewCell, showCalcButtonAction dataRatesToCalc: RatesDetail)
+}
+
+
 class DetailViewCell: UITableViewCell {
 
     @IBOutlet weak var noLabelDetailViewCell: UILabel!
     @IBOutlet weak var dateLabelDetailViewCell: UILabel!
     @IBOutlet weak var midLabelDetailViewCell: UILabel!
+    
+    var dataRatesToCalc: RatesDetail?
+    weak var delegate: DetailCellDelegate?
+    
+    @IBOutlet weak var calcButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+            
+        // Add action to perform when the button is tapped
+        self.calcButton.addTarget(self, action: #selector(showCalcButtonAction(_:)), for: .touchUpInside)
+      }
     
     func setDetailCell(data: RatesDetail, code: String){
         noLabelDetailViewCell.text = data.no
@@ -19,6 +37,11 @@ class DetailViewCell: UITableViewCell {
         midLabelDetailViewCell.text = "1 \(code) = \(data.mid) PLN"
     }
     
-
-    
+    @IBAction func showCalcButtonAction(_ sender: Any) {
+        if let dataRatesToCalc = dataRatesToCalc {
+            if let delegate = delegate {
+                delegate.detailCell(self, showCalcButtonAction: dataRatesToCalc)
+                }
+        }
+    }
 }
